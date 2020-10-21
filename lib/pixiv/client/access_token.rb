60b38c56bfdbc257520c71bdb1c0d25f1b92bd6f
NightFeather <ss900328@gmail.com>
2020-10-21 20:49:30 +0800

    Update the access token retriver
    to match authorization flow on old endpoints

--- lib/pixiv/client/access_token.rb
+++ lib/pixiv/client/access_token.rb
@@ -1,11 +1,14 @@
 require 'securerandom'
+require 'time'
+require 'openssl'
 
 module Pixiv
   class Client
 
     class AccessToken
-      CLIENT_ID  = "BVO2E8vAAikgUBW8FYpi6amXOjQj".freeze
-      CLIENT_SECRET = "LI1WsFUDrrquaINOdarrJclCrkTtc3eojCOswlog".freeze
+      CLIENT_ID  = "MOBrBDS8blbauoSck0ZfDbtuzpyT".freeze
+      CLIENT_SECRET = "lsACyCD94FhDUtGTXi3QzcFE2uU1hqtDaKeqrdwj".freeze
+      OTP_KEY = '28c1fdd170a5204386cb1313c7077b34f83e4aaf4aa829ce78c231e05b0bae2c'.freeze
       OAUTH_ENDPOINT = "https://oauth.secure.pixiv.net/auth/token".freeze
 
       def initialize user, pass, device_token = nil
@@ -22,6 +25,12 @@ module Pixiv
         self
       end
 
+      def generate_otp
+        timestamp = Time.now.iso8601
+        hashtoken = OpenSSL::Digest::MD5.hexdigest timestamp+OTP_KEY
+        return [timestamp, hashtoken]
+      end
+
       def sign req
         refresh if expired?
         req.add_field "Authorization", "Bearer %s" % [ @access_token ]
@@ -29,20 +38,33 @@ module Pixiv
       end
 
       def login user, pass
-        resp = Net::HTTP.post_form URI(OAUTH_ENDPOINT),
-                                   client_id:     CLIENT_ID,
-                                   client_secret: CLIENT_SECRET,
-                                   grant_type:    "password",
-                                   username:      user,
-                                   password:      pass,
-                                   device_token:  @device_token,
-                                   get_secure_url: true
+        uri = URI(OAUTH_ENDPOINT)
+        otp_ts, otp_hash = generate_otp
+        req = Net::HTTP::Post.new uri, {
+          'User-Agent': 'PixivAndroidApp/5.0.115 (Android 6.0; PixivBot)',
+          'X-Client-Time': otp_ts,
+          'X-Client-Hash': otp_hash
+        }
+        req.form_data = {
+          client_id:     CLIENT_ID,
+          client_secret: CLIENT_SECRET,
+          grant_type:    "password",
+          username:      user,
+          password:      pass,
+          device_token:  @device_token,
+          get_secure_url: true
+        }
+
+        Net::HTTP.start uri.host, uri.port, use_ssl: uri.port == 443 do |http|
+
+          httpresp = http.request req
+          raise AuthorizationFailed, httpresp.body unless %w{ 200 301 302 }.include? httpresp.code
   
-        raise AuthorizationFailed, resp.body unless %w{ 200 301 302 }.include? resp.code
-  
-        resp = JSON.parse(resp.body)
-        @expire = Time.now + resp["response"]["expires_in"].to_i
-        @access_token = resp["response"]["access_token"]
+          resp = JSON.parse(httpresp.body)
+          @expire = Time.now + resp["response"]["expires_in"].to_i
+          @access_token = resp["response"]["access_token"]
+        end
+        @access_token
       end
 
       def to_s
